305e7ba5d3213347f9bb630c3296feb3c85cdb13
matias lencina <mlencinaa3@gmail.com>
2024-08-22 10:26:18 -0300

    prim commit

---
+++ proyecto_angular/src/app/comp1/comp1.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp1',
+  templateUrl: './comp1.component.html',
+  styleUrls: ['./comp1.component.css']
+})
+export class Comp1Component {
+
+  public empleados: any = [
+    {
+    nombre: "Pedro",
+    pasante: false,
+    edad: 25,
+    datos: {
+    puesto: "Programación",
+    antiguedad: 3
+    }
+    },
+    {
+    nombre: "Julieta",
+    pasante: false,
+    edad: 28,
+    datos: {
+    puesto: "Programación",
+    antiguedad: 1
+    }
+    },
+    {
+    nombre: "Joaquin",
+    pasante: true,
+    edad: 20,
+    datos: {
+    puesto: "Programación",
+    antiguedad: 1
+    }
+    },
+    {
+    nombre: "Ricardo",
+    pasante: false,
+    edad: 50,
+    datos: {
+    puesto: "Ventas",
+    antiguedad: 5
+    }
+    },
+    {
+    nombre: "Fabio",
+    pasante: true,
+    edad: 22,
+    datos: {
+    puesto: "Programación",
+    antiguedad: 0.5
+    }
+    }
+    ];
+    
+
+    onClick(nombre: string){
+      console.log(`hola ${nombre}`);
+    }
+    
+}
